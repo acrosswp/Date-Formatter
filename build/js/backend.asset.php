c4bc9139aaa51ec62c63fc8d10da73853e3605ba
raftaar1191 <raftaar1191@gmail.com>
2024-08-21 21:27:18 +0530

    Update code

--- build/js/backend.asset.php
+++ build/js/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'fe61d46d801e0652270f');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => 'c924698fb87ed9f94c8f');
